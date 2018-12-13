f9177d938cc092505c417e66727d9595171d52a4
Marko <mpecic2010@gmail.com>
2018-12-13 16:09:44 -0700

    stop closing VS CODE by mistake

--- src/App.js
+++ src/App.js
@@ -8,26 +8,32 @@ import './App.css';
 
 class App extends Component {
   state = {
-    books: []
+    books: [
+      { id:55, name: "Display Book", price: 99.99, inCart:true}
+    ],
+    itemsInCart: []
   }
   async componentDidMount(){
     const bookList = await fetch('http://localhost:8082/api/books')
     const json = await bookList.json()
-    this.setState({
-      books: json
+    this.setState(prevState => {
+      books:{
+        ...prevState.books, 
+        json
+      }
     })
   }
+  console.log(this.state)
   render() {
     return (
       <div className="App">
-
           <Header />
             <Row>
             <Col xs="10">
               <ListOfBooks booksList={this.state.books} />
             </Col>
             <Col xs="2">
-              <Checkout booksList={this.state.books} />
+              <Checkout booksList={this.state.books} itemsInCart={this.state.books.filter(item => item.inCart)} />
             </Col>
           </Row>
         <Footer />
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -3,24 +3,22 @@ import { Badge, ListGroup, ListGroupItem, Button} from "reactstrap"
 
 class Checkout extends Component {
     render = () => {
-        let booksList = this.props.booksList
-        let itemsSelected = [
-            {id:1, price: 5, title: "Learning JavaScript Design Patterns"},
-            {id:2, price: 15, title: "Learning JavaScript, Cuz BS"}
-        ]
-        let numberOfItemsInCart = Object.keys(itemsSelected).length
+        let numberOfItemsInCart = 0;
+        if (Object.keys(this.props.itemsInCart).length > 0) {
+            numberOfItemsInCart=(Object.keys(this.props.itemsInCart).length)
+        }
         return (
             <>
                 <h5 style={{marginTop: 15}}>
                 Your Shopping Cart <Badge color="success">{numberOfItemsInCart}</Badge>
                 </h5>
-                {itemsSelected.map(item => 
+                {this.props.itemsInCart.map(item => 
                 <ListGroup flush>
                     <ListGroupItem>{item.title}</ListGroupItem>
                 </ListGroup> 
                 )}
                 <p>
-                Total cost of items in cart: {itemsSelected.reduce((a,b) => a+b.price, 0)}
+                Total cost of items in cart: {this.props.itemsInCart.reduce((a,b) => a+b.price, 0)}
                 </p>
                 <Button color="success">Checkout</Button>
             </>
